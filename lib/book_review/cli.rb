1c820d7784b61d7428509085afe781e2d6a867e2
Nicole D <nnardella@gmail.com>
2018-12-14 03:12:28 +0000

    updated cli.rb

--- lib/book_review/cli.rb
+++ lib/book_review/cli.rb
@@ -1,7 +1,18 @@
 class BookReview::CLI        #name spacing so it doesn't get confused with soemthing else similiar to it.
   
   def start 
-    puts "hello"
+    puts "Welcome to the Children's Book Review App!"
+    puts "Here are the top 50 books that all kids should read before they are 12 years old: "
+    #scrape all the books - call on the scraper class.
+    #list the books
+    list_books
+    #ask for input
+    #call another method
+    
+  end
+  
+  def list_books
+    
   end
   
 end
