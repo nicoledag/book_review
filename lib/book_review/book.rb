5f193e2b1a1e9db21e7e30e7b1a4341c79220696
Nicole D <nnardella@gmail.com>
2018-12-14 04:09:47 +0000

    added first scraper method

--- lib/book_review/book.rb
+++ lib/book_review/book.rb
@@ -1,3 +1,25 @@
 class BookReview::Book      
+  
+  attr_accessor :title, :author, :short_desc, :review_link, :age
+  
+  @@all = []
+  
+  def initialize(att_hash)
+    att_hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+    self.save
+  end
+  
+  def save
+    @@all << self
+    self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+    
 end
   
