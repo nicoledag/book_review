a4187958f0e0e5b83e508ec875d64187aaa9c0f6
Nicole D <nnardella@gmail.com>
2018-12-17 19:35:16 +0000

    updated scraper review method

--- lib/book_review/scraper.rb
+++ lib/book_review/scraper.rb
@@ -15,14 +15,13 @@ class BookReview::Scraper
       
       doc.each do |book|
         
+        
+        
        attributes = {
 
         url: "https://commonsensemedia.org" << book.css("div.views-field.views-field-title.result-title a").attribute("href").value,
         title: book.css("div.views-field.views-field-title.result-title a")[0].children.text,
         author: book.css("em.field-content")[0].children.text,
-        
-        #need to clean up scraping white space.
-        
         short_desc: book.css("div.views-field.views-field-field-one-liner.one-liner")[0].children.text.strip,
         age: book.css("div.csm-green-age")[0].children.text
         
@@ -36,30 +35,30 @@ end
 
   def self.scrape_review
     
-    review_page = Nokogiri::HTML(open('https://commonsensemedia.org/book-reviews/the-snowy-day'))
-    binding.pry
+    review_page = Nokogiri::HTML(open('https://commonsensemedia.org/book-reviews/tales-of-a-fourth-grade-nothing'))
+   
 
   
-      doc = review_page.css("div.field-items")
+      doc = review_page.css("div.inner-wrapper")
+      
+      # binding.pry
       
      
-      doc.each do |review|    
+      # doc.each do |review|    
   
       
-      attributes = {
+      attributes = {}
 
-   
-        story: review.css("div.field-item.even p")[4].text,
-        any_good: review.css("div.field-item.even p")[6,7].text,
-        what_parents_need_to_know: review.css("div.field-item.even p")[3].text,
-        families_can_talk_about: review.css("div.field-item.even p")[8,9].text,
+        attributes[:what_parents_need_to_know] = doc.css("div.field-item.even p")[0].children.text
+        attributes[:story] = doc.css("div.field-item.even p")[1].children.text
+        attributes[:any_good] = doc.css("div.field-item.even p")[3,4].children.text
+        attributes[:families_can_talk_about] = doc.css("div.field-item.even p")[5].children.text
 
-        }
-    
+        
         review = BookReview::Review.new(attributes)
         
       end
-  end
+  # end
   
   
   
