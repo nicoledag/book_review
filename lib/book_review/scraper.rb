94770a9b11ea9646145b680c7aee34c33fc5b8e8
Nicole D <nnardella@gmail.com>
2018-12-18 15:12:44 +0000

    updated scraper review

--- lib/book_review/scraper.rb
+++ lib/book_review/scraper.rb
@@ -39,20 +39,30 @@ end
    
 
   
-      doc = review_page.css("div.inner-wrapper")
+      doc = review_page.css("div.panel-content-mid-main.panel-panel")
       
-      # binding.pry
+      binding.pry
       
      
       # doc.each do |review|    
   
       
       attributes = {}
+      
+      
+        attributes[:story] = doc.css("div.shutter-summary-pane.panel-pane.pane-entity-field.pane-node-field-what-is-story p").text
+        
+        attributes[:what_parents_need_to_know] = doc.css("div.shutter-summary-pane.panel-pane.pane-entity-field.pane-node-field-parents-need-to-know p").text
+        
+        attributes[:any_good] = doc.css("div.shutter-summary-pane.panel-pane.pane-entity-field.pane-node-field-any-good p").text
 
-        attributes[:what_parents_need_to_know] = doc.css("div.field-item.even p")[0].children.text
-        attributes[:story] = doc.css("div.field-item.even p")[1].children.text
-        attributes[:any_good] = doc.css("div.field-item.even p")[3,4].children.text
-        attributes[:families_can_talk_about] = doc.css("div.field-item.even p")[5].children.text
+        attributes[:families_can_talk_about] = doc.css("div.shutter-summary-pane.panel-pane.pane-entity-field.pane-node-field-family-topics p").text
+
+
+        # attributes[:what_parents_need_to_know] = doc.css("div.field-item.even p")[0].children.text
+        # attributes[:story] = doc.css("div.field-item.even p")[1].children.text
+        # attributes[:any_good] = doc.css("div.field-item.even p")[3,4].children.text
+        # attributes[:families_can_talk_about] = doc.css("div.field-item.even p")[5].children.text
 
         
         review = BookReview::Review.new(attributes)
