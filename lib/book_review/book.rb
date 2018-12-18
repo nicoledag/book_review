3cb4faba95fade36a456b74d81f89f2333ba0537
Nicole D <nnardella@gmail.com>
2018-12-18 17:13:00 +0000

    updated book class

--- lib/book_review/book.rb
+++ lib/book_review/book.rb
@@ -1,7 +1,8 @@
 
 class BookReview::Book
 
-  attr_accessor :title, :author, :short_desc, :url, :age, :review
+  attr_accessor :title, :author, :short_desc, :url, :age
+  attr_reader :review  #reader for @review array
 
   @@all = []
 
@@ -22,6 +23,15 @@ class BookReview::Book
   def self.all
     @@all
   end
+
+  def add_review(review)   #assigns the review to the book then adds the review to the @review array.
+    review.book = self unless review.book  #assigns the current book to the review's 'book' property.  (review belongs to book)
+    #does not assign the book if the review already has an book.
+    @review << review unless review.include?(review)
+    #adds the review to the current book's 'review' array.
+    #does not add the review if the already exists.
+  end
+
 
 
 end
